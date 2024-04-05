1a17e1e593b76ec54abc61fd77084509bb11997a
0wissem <wissem.ksentini@gmail.com>
2024-04-05 22:24:00 +0200

    search bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,49 @@
+import {Image, StyleSheet, TextInput, View} from 'react-native';
+import React from 'react';
+import { COLORS } from '../constants/colors';
+
+
+interface ISearchBar {
+  value: string;
+  onChangeText: (value: string) => void;
+}
+
+const SearchBar: React.FC<ISearchBar> = ({onChangeText, value}) => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/icons/loop.png')} style={styles.icon} />
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={styles.textInput}
+        cursorColor={COLORS.GRAY_TEXT}
+        placeholder="Search"
+        placeholderTextColor={COLORS.GRAY_TEXT}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.GRAY_BACKGROUND,
+    height: 50,
+    marginHorizontal: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 10,
+    paddingHorizontal: 7,
+  },
+  icon: {
+    height: 20,
+    width: 20,
+  },
+  textInput: {
+    flexGrow: 1,
+    height: 30,
+    paddingStart: 7,
+    color: COLORS.GRAY_TEXT,
+  },
+});
