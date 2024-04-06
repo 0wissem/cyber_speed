a1521e36ba83af4c1d0fc28966b0b26ca9c508f3
0wissem <wissem.ksentini@gmail.com>
2024-04-06 07:49:56 +0200

    remove unecceary data and imports

--- src/screens/Movie.tsx
+++ src/screens/Movie.tsx
@@ -1,24 +1,15 @@
-import {
-  ActivityIndicator,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import Poster from '../components/Poster';
-import {COLORS} from '../constants/colors';
-import Keyword from '../components/Keyword';
-import Title from '../components/Title';
-import Description from '../components/Description';
-import Actor from '../components/Actor';
 import {observer} from 'mobx-react-lite';
 import {tmbdStore} from '../stores/tmbd';
 import {getImageFullUri} from '../utils/helpers';
+import Poster from '../components/Poster';
 import Loader from '../components/Loader';
-const description =
-  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+import Keyword from '../components/Keyword';
+import Title from '../components/Title';
+import {COLORS} from '../constants/colors';
+import Description from '../components/Description';
+import Actor from '../components/Actor';
 const Movie = ({navigation, route}) => {
   const {retreiveMovieDetails, movie} = tmbdStore;
   const movie_id = route?.params?.id;
