a657d314da46b33e62cc3ce3d045ea66f0c20d3b
0wissem <wissem.ksentini@gmail.com>
2024-04-06 03:05:28 +0200

    home header

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,61 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {COLORS} from '../constants/colors';
+import FastImage from 'react-native-fast-image';
+import {AVATAR} from '../constants/images';
+import {STRINGS} from './strings';
+
+interface IHomeHeader {
+  label?: string;
+  description?: string;
+  avatar?: string;
+}
+const HomeHeader: React.FC<IHomeHeader> = ({
+  label = STRINGS.HELLO,
+  description = STRINGS.DESCRIPTION_HEADER,
+  avatar = AVATAR,
+}) => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.subContainer}>
+      <FastImage source={avatar} style={styles.avatar} />
+      <View style={styles.texts}>
+        <Text style={styles.name}>{label}</Text>
+        <Text style={styles.desciption}>{description}</Text>
+      </View>
+    </View>
+  </SafeAreaView>
+);
+
+export default HomeHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BLACK,
+  },
+  subContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    height: 50,
+    paddingHorizontal: 10,
+  },
+  avatar: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+  },
+  texts: {
+    marginStart: 10,
+  },
+  name: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: '600',
+  },
+  desciption: {
+    color: '#FFF',
+    fontSize: 13,
+    fontWeight: '400',
+  },
+});
---
+++ src/components/strings.ts
@@ -0,0 +1,6 @@
+export const STRINGS = {
+  HELLO:"Hi, Brother!",
+  DESCRIPTION_HEADER: "Find your Movie",
+  SHOW_LESS:"show less..",
+  SHOW_MORE:"show more.."
+}
