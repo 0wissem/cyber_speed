025876d3382ea449f66fcf74d77f6224263a376d
0wissem <wissem.ksentini@gmail.com>
2024-04-06 07:37:51 +0200

    tmbd store

---
+++ src/stores/tmbd.ts
@@ -0,0 +1,116 @@
+import {observable, runInAction, action, makeAutoObservable} from 'mobx';
+interface IGenre {
+  id: number;
+  name: string;
+}
+interface IMovie {
+  overview: string | undefined;
+  poster_path: string | undefined;
+  genres: Array<IGenre>;
+  title: string;
+  vote_average: number;
+}
+export class TmbdStore {
+  movies = [];
+  accessToken = '';
+  defaultPageContent = 10;
+  movie: IMovie | null = null;
+  constructor() {
+    makeAutoObservable(this, {
+      movies: observable,
+      setAccessToken: action,
+      reset: action,
+      searchMovies: action,
+      fetchRandomMovies: action,
+      setDefaultPageContent: action,
+    });
+  }
+
+  setAccessToken = (token: string) => {
+    runInAction(() => {
+      this.accessToken = token;
+    });
+  };
+  setDefaultPageContent = (perPage: number) => {
+    this.defaultPageContent = perPage;
+  };
+
+  fetchRandomMovies = async () => {
+    const url = `https://api.themoviedb.org/3/trending/movie/day?per_page=${this.defaultPageContent}&language=en-US`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+    };
+
+    fetch(url, options)
+      .then(res => {
+        return res.json();
+      })
+      .then(data =>
+        runInAction(() => {
+          this.movies = data?.results;
+          // can be used to handle states instead of using "movies" observable.
+          // also helpfull for pagination to keep tracking the page number.
+          return data;
+        }),
+      );
+  };
+  searchMovies = async (query: string | undefined) => {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${this.accessToken}`,
+      },
+    };
+
+    fetch(url, options)
+      .then(res => {
+        return res.json();
+      })
+      .then(data =>
+        runInAction(() => {
+          this.movies = data?.results;
+          // can be used to handle states instead of using "movies" observable.
+          // also helpfull for pagination to keep tracking the page number.
+          return data;
+        }),
+      );
+  };
+  retreiveMovieDetails = async (movie_id: number) => {
+    try {
+      if (!movie_id) throw 'Missing ID';
+      const url = `https://api.themoviedb.org/3/movie/${movie_id}?language=en-US`;
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+      };
+      fetch(url, options)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          runInAction(() => {
+            this.movie = data;
+            // can be used to handle state instead of using "movie" observable.
+            return data;
+          });
+        });
+    } catch (error) {}
+  };
+
+  reset = () => {
+    this.movies = [];
+    this.movie = null;
+    this.accessToken = ' ';
+  };
+}
+
+export const tmbdStore = new TmbdStore();
