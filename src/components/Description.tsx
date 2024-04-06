0141277aa821e9669f64fee45765a0b8cf3564f2
0wissem <wissem.ksentini@gmail.com>
2024-04-06 02:05:24 +0200

    add Description component

---
+++ src/components/Description.tsx
@@ -0,0 +1,60 @@
+import {
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {COLORS} from '../constants/colors';
+
+interface IDescription {
+  label: string;
+}
+const Description: React.FC<IDescription> = ({label = ''}) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  const toggleDescriptionView = useCallback(() => {
+    setShowFullDescription(p => !p);
+  }, []);
+
+  return (
+    <LinearGradient
+      start={{x: 0, y: 1}}
+      end={{x: 0, y: 0}}
+      colors={[
+        `${COLORS.RED}11`,
+        `${COLORS.RED}44`,
+        `${COLORS.RED}11`,
+        '#00000022',
+      ]}>
+      <Text style={styles.label} numberOfLines={showFullDescription ? null : 4}>
+        {label}
+      </Text>
+      <TouchableOpacity
+        onPress={toggleDescriptionView}
+        hitSlop={{top: 40, right: 20, left: 20}}>
+        {showFullDescription ? (
+          <Text style={styles.moreOrLess}>show less..</Text>
+        ) : (
+          <Text style={styles.moreOrLess}>more..</Text>
+        )}
+      </TouchableOpacity>
+    </LinearGradient>
+  );
+};
+
+export default Description;
+
+const styles = StyleSheet.create({
+  label: {
+    color: '#FFF',
+  },
+  moreOrLess: {
+    color: COLORS.RED,
+    textTransform: 'capitalize',
+    alignSelf: 'center',
+    fontSize: 12,
+  },
+});
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,4 +2,5 @@ export const COLORS = {
   GRAY_TEXT: '#b8b6b6',
   GRAY_BACKGROUND: '#b8b6b622',
   BLACK: '#0E1016',
+  RED: '#bd3e6d',
 };
